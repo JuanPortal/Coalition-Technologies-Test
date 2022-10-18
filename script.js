de91f057b3c1aeffdb399098b37148c68842d5db
Portal <juanportalg@gmail.com>
2022-10-18 16:07:47 -0500

    carousel completed

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ window.addEventListener('scroll', headerOnScroll)
 
 // CAROUSEL 
 
+const slider1 = document.querySelector('#slider1')
+const slider2 = document.querySelector('#slider2')
+const slider3 = document.querySelector('#slider3')
+const sliders = [slider1, slider2, slider3]
+const imgSection = document.querySelector('.img-section')
 
+slider1.addEventListener('click', () => {
+    imgSection.style.alignItems = 'flex-start'
+    sliders.forEach(slider => {
+        slider.style.backgroundColor = '#636d81'
+    })
+    slider1.style.backgroundColor = '#fff'
+})
+
+slider2.addEventListener('click', () => {
+    imgSection.style.alignItems = 'center'
+    sliders.forEach(slider => {
+        slider.style.backgroundColor = '#636d81'
+    })
+    slider2.style.backgroundColor = '#fff'
+})
+
+slider3.addEventListener('click', () => {
+    imgSection.style.alignItems = 'flex-end'
+    sliders.forEach(slider => {
+        slider.style.backgroundColor = '#636d81'
+    })
+    slider3.style.backgroundColor = '#fff'
+})
 
 // ADD TRANSITION
